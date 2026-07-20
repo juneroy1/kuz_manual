df5e29df7fbbcee6f445bc8b46c570c3e74d6d53
Juneroy Quinimon <eroyjune@gmail.com>
2026-07-21 00:15:48 +0800

    feat: added route api for forgot and reset password

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
