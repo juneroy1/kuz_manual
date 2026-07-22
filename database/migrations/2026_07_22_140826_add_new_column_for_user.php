7b79d66b2ac6fb9e9bb1a2d36e85f159c92d04f4
Juneroy Quinimon <eroyjune@gmail.com>
2026-07-22 22:10:40 +0800

    feat added new migration for users profile and bio

---
+++ database/migrations/2026_07_22_140826_add_new_column_for_user.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username')->nullable()->unique()->after('name');
+            $table->text('bio')->nullable()->after('password');
+            $table->string('profile_photo')->nullable()->after('bio');
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn(['username', 'bio', 'profile_photo']);
+        });
+    }
+};
