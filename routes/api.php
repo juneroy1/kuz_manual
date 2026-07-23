c5a7292f4b83e14adbf11d542addf27847968888
Juneroy Quinimon <eroyjune@gmail.com>
2026-07-23 13:26:59 +0800

    feat added new route api for get and put profile update

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -8,4 +9,8 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
 Route::post('/reset-password', [AuthController::class, 'resetPassword']);
+Route::get('/profile', [ProfileController::class, 'show']);
+Route::put('profile', [ProfileController::class, 'update']);
+
+
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
